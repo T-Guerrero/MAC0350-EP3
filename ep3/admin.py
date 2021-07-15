6532da695cd53c944e2a24c93199fe18d49e801c
Thiago Guerrero <tgbalera@hotmail.com>
2021-07-14 21:49:23 -0300

    Create models
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- ep3/admin.py
+++ ep3/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class AmostraInline(admin.TabularInline):
+    model = Amostra
+    extra = 1
+
+
+class ExameWithAmostra(admin.ModelAdmin):
+    inlines = (AmostraInline,)
+
+
+admin.site.register(Paciente)
+admin.site.register(Exame, ExameWithAmostra)
+admin.site.register(Amostra)
--- ep3/models.py
+++ ep3/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.db.models.lookups import LessThan
 
 # Create your models here.
+
+
+class Paciente(models.Model):
+    cpf = models.CharField(max_length=14, unique=True)
+    nome = models.CharField(max_length=255)
+    data_nascimento = models.DateField()
+    endereco = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome + ' - ' + self.cpf
+
+
+class Exame(models.Model):
+    EXAM_TYPES = (
+        ('PCR', 'PCR'),
+        ('Anticorpos', 'Anticorpos'),
+    )
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    virus = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=10, choices=EXAM_TYPES)
+    data_solicitacao = models.DateTimeField('Data de Solicitação')
+    data_execucao = models.DateTimeField('Data de Execução')
+
+    def __str__(self):
+        return self.paciente.cpf + ' - ' + self.virus + ' | ' + str(self.data_solicitacao) + ' - ' + str(self.data_execucao)
+
+
+class Amostra(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    exame = models.ForeignKey(Exame, on_delete=models.CASCADE)
+    data_coleta = models.DateTimeField('Data de Coleta')
+    tipo_material = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.paciente.cpf + ' - ' + self.exame.tipo + '/' + self.exame.virus + ' | ' + str(self.data_coleta)
