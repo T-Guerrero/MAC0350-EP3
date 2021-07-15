deaedc642ea2f6ede85440e2248e9956df03cf4c
Thiago Guerrero <tgbalera@hotmail.com>
2021-07-15 20:07:25 -0300

    Add show page for all entities
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- ep3/models.py
+++ ep3/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.lookups import LessThan
 
 # Create your models here.
 
--- ep3/views.py
+++ ep3/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.db import connection
+
 from .models import Paciente, Exame, Amostra
 # Create your views here.
+
+cursor = connection.cursor()
 
 
 def index(req):
@@ -23,7 +27,25 @@ def pacientes(req):
 
 
 def paciente(req, id):
-    return
+    exames_query = ' \
+                    SELECT E.id, E.data_solicitacao, E.data_execucao, E.tipo, E.virus \
+                    FROM ep3_exame as E  \
+                    WHERE E.paciente_id = %s \
+                    '
+    amostras_query = ' \
+                    SELECT A.id, A.exame_id, A.data_coleta, A.tipo_material \
+                    FROM ep3_amostra as A \
+                    WHERE A.paciente_id = %s \
+                    '
+    paciente = get_object_or_404(Paciente, id=id)
+    exames = runSQLQuery(exames_query, (id,))
+    amostras = runSQLQuery(amostras_query, (id,))
+    context = {
+        'paciente': paciente,
+        'exames': exames,
+        'amostras': amostras
+    }
+    return render(req, 'paciente.html', context)
 
 
 def exames(req):
@@ -32,7 +54,18 @@ def exames(req):
 
 
 def exame(req, id):
-    return
+    amostras_query = ' \
+                    SELECT A.id, A.data_coleta, A.tipo_material \
+                    FROM ep3_amostra as A \
+                    WHERE A.exame_id = %s; \
+                    '
+    exame = get_object_or_404(Exame, id=id)
+    amostras = runSQLQuery(amostras_query, (id,))
+    context = {
+        'exame': exame,
+        'amostras': amostras
+    }
+    return render(req, 'exame.html', context)
 
 
 def amostras(req):
@@ -41,4 +74,12 @@ def amostras(req):
 
 
 def amostra(req, id):
-    return
+    amostra = get_object_or_404(Amostra, id=id)
+    return render(req, 'amostra.html', {'amostra': amostra})
+
+# Funções auxiliares
+
+
+def runSQLQuery(query, args):
+    cursor.execute(query, args)
+    return cursor.fetchall()
